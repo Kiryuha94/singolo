ad5a146e4311e752912762832ab1e0bcc13e7649
Kirill <kirillplusena@gmail.com>
2020-11-28 18:52:34 +0300

    feat: interactivity for button

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   tags.forEach((e) => {
     e.addEventListener('click', () => {
+     console.log(e);
+     tags.forEach( (element) => {
+      element.classList.remove('tag__active')
+     })
+
+     e.classList.add('tag__active')
       for (let i = imges.length; i >= 0; i--) {
         portfolioImg.appendChild(imges[(Math.random() * i) | 0])
       }
